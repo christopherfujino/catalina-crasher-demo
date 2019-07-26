6717cd5b2f7d1696e22ccb8332e3322c12b4cb78
Christopher Fujino <christopherfujino@gmail.com>
2019-07-26 12:42:56 -0700

    cleanup

--- main.dart
+++ main.dart
@@ -1,29 +1,13 @@
 import 'dart:io';
 
-//const String oldBin = 'dart-old';
 const String newBin = 'dart-new';
 const String targetBin = 'dart';
-const String helloWorldFile = 'helloWorld.dart';
 
 void main() {
-  //print('main start...');
-  //ls();
-  //dartVersion();
-
   print('deleting $targetBin file...');
   File(targetBin).deleteSync();
 
   printResults(Process.runSync('ps', []));
-
-  //print('replacing dart with new version');
-  //final File newDart = File(newBin);
-  //newDart.copySync(targetBin);
-
-  //ls();
-  //dartVersion();
-
-  //print('invoking second dart script...');
-  //helloWorld();
 }
 
 void printResults(ProcessResult pr) {
@@ -34,17 +18,4 @@ void printResults(ProcessResult pr) {
     '  stdout:\n${pr.stdout}'
     '  stderr:\n${pr.stderr}'
   );
-}
-
-void ls() {
-  var pr = Process.runSync('uname', []);
-  printResults(pr);
-}
-
-void dartVersion() {
-  printResults(Process.runSync(targetBin, ['--version']));
-}
-
-void helloWorld() {
-  printResults(Process.runSync(targetBin, [helloWorldFile]));
 }
